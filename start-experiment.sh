b341f8088e28301f49a80757d777af91c90a8176
Pranay Mundra <pranaymundra4510@gmail.com>
2024-08-28 19:47:26 -0400

    change partition to day and increase mem

--- start-experiment.sh
+++ start-experiment.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=21
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=16G
 
 #sh load-modules.sh
 ml Go/1.21.4 OpenMPI/4.1.4-GCC-12.2.0
