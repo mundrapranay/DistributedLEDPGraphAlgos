b13b1599b6485304d4e9e57bad68da8bc18babf6
Pranay Mundra <pranaymundra4510@gmail.com>
2024-08-29 14:53:08 -0400

    increased mem

--- start-experiment.sh
+++ start-experiment.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=64
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=16G
+#SBATCH --mem-per-cpu=8G
 
 #sh load-modules.sh
 ml Go/1.21.4 OpenMPI/4.1.4-GCC-12.2.0
