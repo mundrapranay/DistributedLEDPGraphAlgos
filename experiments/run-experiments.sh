3fc84a83efc91841116ee7ea3c8ff3560eb21aa9
Pranay Mundra <pranaymundra4510@gmail.com>
2024-12-18 18:21:22 -0500

    scripts for gcp

--- experiments/run-experiments.sh
+++ experiments/run-experiments.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-
-for graph in 'email-eu-core' 'wiki' 'enron' 'brightkite' 'ego-twitter' 'gplus' 'stanford' 'dblp' 'brain' 'orkut' 'livejournal' 'twitter' 'friendster'
+for graph in 'brain' 'orkut' 'livejournal' 'twitter' 'friendster'
+# for graph in 'email-eu-core' 'wiki' 'enron' 'brightkite' 'ego-twitter' 'gplus' 'stanford' 'dblp' 'brain' 'orkut' 'livejournal' 'twitter' 'friendster'
 do
-    for alg in 'kcoreLDP' 'triangle_countingLDP'
+    # for alg in 'kcoreLDP' 'triangle_countingLDP'
+    for alg in 'kcoreLDP'
     do
         cd ../scripts/
         echo "Partitioining Graph: $graph"
