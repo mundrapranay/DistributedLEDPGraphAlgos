fe7c6dce0747bc7c60fd3bac3b6e6725d994a05b
Pranay Mundra <pranaymundra4510@gmail.com>
2025-03-06 02:21:11 +0530

    clean install

--- setup.sh
+++ setup.sh
@@ -73,41 +73,27 @@ go version
 
 # Generate YAML configuration files for experiments
 echo "Generating configuration files..."
-declare -A GRAPH_SIZES=(
-    ["email-eu-core"]=986
-    ["wiki"]=7115
-    ["enron"]=36692
-    ["brightkite"]=58228
-    ["ego-twitter"]=81306
-    ["gplus"]=107614
-    ["stanford"]=281903
-    ["dblp"]=317080
-    ["brain"]=784262
-    ["orkut"]=3072441
-    ["livejournal"]=4846609
-    ["twitter"]=41652230
-    ["friendster"]=65608366
-)
-ALGO_NAMES=("kcoreLDP" "triangle_countingLDP")
+graphs=("email-eu-core" "wiki" "enron" "brightkite" "ego-twitter" "gplus" "stanford" "dblp" "brain" "orkut" "livejournal" "twitter" "friendster")
+graph_sizes=(986 7115 36692 58228 81306 107614 281903 317080 784262 3072441 4846609 41652230 65608366)
 
-for graph in "${!GRAPH_SIZES[@]}"; do
-    for algo in "${ALGO_NAMES[@]}"; do
-        CONFIG_FILE="./experiments/config/${graph}-${algo}.yaml"
-        cat > "$CONFIG_FILE" <<EOF
-graph: ${graph}
-graph_size: ${GRAPH_SIZES[$graph]}
-algo_name: ${algo}
-num_workers: 80
-epsilon: 0.5
-phi: 0.5
-runs: 1
-bias: true
-bias_factor: 8
-noise: true
-output_file_tag: hpc_baseline_true
-graph_loc: ../graph-dp-experiments/graphs/
-EOF
-        echo "Created config file: $CONFIG_FILE"
+for index in "${!graphs[@]}"; do
+    graph=${graphs[$index]}
+    # Create N files for each graph
+    for alg in 'kcoreLDP' 'triangle_countingLDP'; do
+        filename="${graph}-${alg}.yaml"
+        echo "graph: ${graph}" > "$filename"
+        echo "graph_size: ${graph_sizes[$index]}" >> "$filename"
+        echo "algo_name: ${alg}" >> "$filename"
+        echo "num_workers: 80" >> "$filename"
+        echo "epsilon: 0.5" >> "$filename"
+        echo "phi: 0.5" >> "$filename"
+        echo "runs: 1" >> "$filename"
+        echo "bias: true" >> "$filename"
+        echo "bias_factor: 8" >> "$filename"
+        echo "noise: true" >> "$filename"
+        echo "output_file_tag: hpc_baseline_true" >> "$filename"
+        echo "graph_loc: ../graph-dp-experiments/graphs/" >> "$filename"
+        echo "Created $filename"
     done
 done
 
