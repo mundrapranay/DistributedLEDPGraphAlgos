c6669d9b7b245b75c09ac81e50a845bca6e2697a
Pranay Mundra <pranaymundra4510@gmail.com>
2025-05-16 19:14:27 -0400

    tcount parallel launch

--- experiments/rr-kcore-exp.sh
+++ experiments/rr-kcore-exp.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #SBATCH --job-name=rr-kcore
-#SBATCH --partition=scavenge
-#SBATCH --time=1-00:00:00
+#SBATCH --partition=week
+#SBATCH --time=3-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=48
+#SBATCH --cpus-per-task=64
 #SBATCH --mem=0
 
 ml Go
 
 cd ../cmd/
-# for graph in 'brain' 'orkut' 'livejournal' 'twitter' 'friendster'
+
 for graph in 'brain' 'orkut' 'livejournal' 'twitter' 'friendster'
 do
      for alg in 'rr-kcore'
