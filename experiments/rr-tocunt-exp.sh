b72562fd16d00b633b587cd772984e645106fb86
Pranay Mundra <pranaymundra4510@gmail.com>
2025-05-16 19:06:04 -0400

    tcount parallel launch

--- experiments/rr-tocunt-exp.sh
+++ experiments/rr-tocunt-exp.sh
@@ -2,20 +2,23 @@
 #SBATCH --job-name=rr-tcount-bigmem
 #SBATCH --partition=bigmem
 #SBATCH --time=1-00:00:00
-#SBATCH --nodes=2
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --nodes=3
+#SBATCH --ntasks=3
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=13
 #SBATCH --mem=0
+#SBATCH --array=0-2
 
 ml Go
 
 cd ../cmd/
-# for graph in 'brain' 'orkut' 'livejournal' 'twitter' 'friendster'
-for graph in  'stanford' 'dblp' 'gplus'
-do
-     for alg in 'rr-tcount'
-    do
-        echo "Running Experiments: $graph"
-        go run main.go --config_file ${graph}-${alg}.yaml --workers 80
-    done
-done
+
+# Define the graphs as an array
+graphs=('stanford' 'dblp' 'gplus')
+alg='rr-tcount'
+
+# Get the current graph based on array task ID
+current_graph=${graphs[$SLURM_ARRAY_TASK_ID]}
+
+echo "Running Experiments for graph: $current_graph"
+go run main.go --config_file ${current_graph}-${alg}.yaml --workers 80
